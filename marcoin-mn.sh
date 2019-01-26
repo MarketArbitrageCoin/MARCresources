455e6f0f0721104cc6e138c0ee017059775af246
Market Arbitrage Coin <43622087+MarketArbitrageCoin@users.noreply.github.com>
2019-01-26 19:29:32 +0200

    Update marcoin-mn.sh

--- marcoin-mn.sh
+++ marcoin-mn.sh
@@ -42,7 +42,7 @@ function update_node() {
     $COIN_PATH$COIN_CLI stop >/dev/null 2>&1
     sleep 10 >/dev/null 2>&1
     rm $COIN_PATH$COIN_DAEMON $COIN_PATH$COIN_CLI >/dev/null 2>&1
-#    rm -r $CONFIGFOLDER/{backups,blocks,budget.dat,chainstate,database,db.log,fee_estimates.dat,mncache.dat,mnpayments.dat,peers.dat,sporks,zerocoin} >/dev/null 2>&1
+    rm -r $CONFIGFOLDER/{backups,blocks,budget.dat,chainstate,database,db.log,fee_estimates.dat,mncache.dat,mnpayments.dat,peers.dat,sporks,zerocoin} >/dev/null 2>&1
     download_node
     #update_config
     configure_systemd
